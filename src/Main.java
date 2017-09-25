03f10fc905ce15e5b71ddaf48776c6fbbd3866d1
Rait <laurik.rait@gmail.com>
2017-09-25 17:02:30 +0300

    Change Main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Tere 3TAR(e)");
-        System.out.println("Tere Raidi poolt");
+        System.out.println("Tere Rait Lauriku poolt");
     }
 }
