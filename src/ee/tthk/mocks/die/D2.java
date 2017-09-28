dc43403738dc95bff1f6218c3754afa7bfdd687a
Enrico <enrico.lund@gmail.com>
2017-09-28 13:51:39 +0300

    Added new dice classes

---
+++ src/ee/tthk/mocks/die/D2.java
@@ -0,0 +1,11 @@
+package ee.tthk.mocks.die;
+import ee.tthk.interfaces.IDie;
+
+/**
+ * Created by opilane on 28.09.2017.
+ */
+public class D2 implements IDie{
+    public int Roll() {
+        return 2;
+    }
+}
---
+++ src/ee/tthk/mocks/die/D4.java
@@ -0,0 +1,11 @@
+package ee.tthk.mocks.die;
+import ee.tthk.interfaces.IDie;
+
+/**
+ * Created by opilane on 28.09.2017.
+ */
+public class D4 implements IDie{
+    public int Roll() {
+        return 4;
+    }
+}
