453c42f366ec525705a1890af302a80db448f063
opilane <salat.js@gmail.com>
2017-09-28 14:05:37 +0300

    Merged master to Player, added comments to IPlayer

--- src/ee/tthk/interfaces/IPlayer.java
+++ src/ee/tthk/interfaces/IPlayer.java
@@ -1,8 +1,37 @@
 package ee.tthk.interfaces;
 
-/**
- * Created by opilane on 25.09.2017.
- */
+
 public interface IPlayer {
+
+
+    /**
+     * Sets the player's name.
+     *
+     * @param name - name that the player wants.
+     * */
     void AddName(String name);
+
+
+    /**
+     * Sets a scoreboard for the player.
+     *
+     * @param scoreboard - scoreboard that the player will be set.
+     * */
+    void SetScoreBoard(IScoreBoard scoreboard);
+
+
+    /**
+     * Returns the player's scoreboard for data checking etc.
+     *
+     * @return - returns the scoreboard
+     * */
+    void GetScoreBoard();
+
+
+    /**
+     * Returns the player's total score from the scoreboard.
+     *
+     * @return - returns totalScore
+     * */
+    int GetScoreBoardTotal();
 }
