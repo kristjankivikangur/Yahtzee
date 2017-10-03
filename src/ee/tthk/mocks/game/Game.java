44a7898400496915e4551c98dbe0f84d69984a05
Tomy Kalm <tomy.kalm@gmail.com>
2017-10-03 16:09:31 +0300

    Added a new Game.java mock class

---
+++ src/ee/tthk/mocks/game/Game.java
@@ -0,0 +1,28 @@
+package ee.tthk.mocks.game;
+
+import ee.tthk.interfaces.*;
+
+/**
+ * Created by opilane on 03.10.2017.
+ */
+public class Game implements IGame {
+    @Override
+    public void StartGame(int NumberOfPlayers) {
+
+    }
+
+    @Override
+    public void ChangeRound() {
+
+    }
+
+    @Override
+    public void ChangePlayer() {
+
+    }
+
+    @Override
+    public void ChooseWinner(IPlayer[] players) {
+
+    }
+}
