a21f8f121b5a077461b2e5306eab4d1801376444
Tomy Kalm <tomy.kalm@gmail.com>
2017-10-03 14:50:22 +0300

    Added Game.java to impls.game package

---
+++ src/ee/tthk/impls/game/Game.java
@@ -0,0 +1,29 @@
+package ee.tthk.impls.game;
+
+import ee.tthk.interfaces.IGame;
+import ee.tthk.interfaces.IPlayer;
+
+/**
+ * Created by opilane on 03.10.2017.
+ */
+public class Game implements IGame {
+    @Override
+    public void StartGame(int NumberOfPlayers) {
+
+    }
+
+    @Override
+    public void ChangeRound() {
+
+    }
+
+    @Override
+    public void ChangePlayer() {
+
+    }
+
+    @Override
+    public void ChooseWinner(IPlayer[] players) {
+
+    }
+}
