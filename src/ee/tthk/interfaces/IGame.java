e0233f552d69c0f8242897cde1e835e0b639d5be
Tomy Kalm <tomy.kalm@gmail.com>
2017-10-04 09:33:07 +0300

    All methods take in a HashMap of players

--- src/ee/tthk/interfaces/IGame.java
+++ src/ee/tthk/interfaces/IGame.java
@@ -10,21 +10,21 @@ public interface IGame {
      * Asks the player for the number of players and then calls out the Player class for it to give them names
      * @param NumberOfPlayers - The number of players
      */
-    void StartGame(int NumberOfPlayers);
+    void StartGame(int NumberOfPlayers,Map<String,Integer> players);
 
     /**
      * Changes the round if it has ended and all the players have played their current round
      */
-    void ChangeRound();
+    void ChangeRound(Map<String,Integer> players);
 
     /**
      * Changes the player if the earlier player has ended his/her's round
      */
-    void ChangePlayer();
+    void ChangePlayer(Map<String,Integer> players);
 
     /**
      * Chooses the winner of the game after all the rounds have been played
      * @param players - An array of player
      */
-    void ChooseWinner(Map players);
+    void ChooseWinner(Map<String, Integer> players);
 }
--- src/ee/tthk/mocks/game/Game.java
+++ src/ee/tthk/mocks/game/Game.java
@@ -2,27 +2,29 @@ package ee.tthk.mocks.game;
 
 import ee.tthk.interfaces.*;
 
+import java.util.Map;
+
 /**
  * Created by opilane on 03.10.2017.
  */
 public class Game implements IGame {
     @Override
-    public void StartGame(int NumberOfPlayers) {
+    public void StartGame(int NumberOfPlayers,Map<String,Integer> players) {
 
     }
 
     @Override
-    public void ChangeRound() {
+    public void ChangeRound(Map<String,Integer> players) {
 
     }
 
     @Override
-    public void ChangePlayer() {
+    public void ChangePlayer(Map<String,Integer> players) {
 
     }
 
     @Override
-    public void ChooseWinner(IPlayer[] players) {
+    public void ChooseWinner(Map<String, Integer> players) {
 
     }
 }
