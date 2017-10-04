82f60a729611aa063c5cd22f366cd9c92f036e68
Tomy Kalm <tomy.kalm@gmail.com>
2017-10-04 09:01:10 +0300

    Completed game implementation for now

--- src/ee/tthk/impls/game/Game.java
+++ src/ee/tthk/impls/game/Game.java
@@ -1,11 +1,10 @@
 package ee.tthk.impls.game;
 
 import ee.tthk.interfaces.*;
+import ee.tthk.mocks.player.Player;
 import ee.tthk.mocks.round.Round;
 
 import java.util.*;
-
-import org.apache.commons.lang3.ArrayUtils;
 /**
  * Created by opilane on 03.10.2017.
  */
@@ -14,40 +13,51 @@ public class Game implements IGame {
     int CurrentPlayer;
     int NrOfPlayers;
     Round round = new Round();
-    Player names = new Player;
+    Player names = new Player();
     Map<String,Integer> players = new HashMap<String, Integer>();
 
     @Override
     public void StartGame(int NumberOfPlayers) {
-        /*players.put("Tomy",25);
-        System.out.println(players);*/
-        for (NumberOfPlayers){
-            players.put(names.SetName,"0");
+        for (int i = 0;i < NumberOfPlayers;i++){
+            players.put(names.SetName(),0);
         }
+        round.start(players);
     }
 
     @Override
     public void ChangeRound() {
-
+        if (CurrentRounds > 13){
+            if (CurrentPlayer < NrOfPlayers){
+                ChangePlayer();
+            }
+            else {
+                CurrentPlayer = 1;
+                round.start(players);
+            }
+        }
+        else {
+            ChooseWinner(players);
+        }
     }
 
     @Override
     public void ChangePlayer() {
-        if (CurrentPlayer < NrOfPlayers){
-            CurrentPlayer+=1;
-            round.start(players);
-        }
-        else {
-            CurrentPlayer = 1;
-            ChangeRound();
-        }
+        CurrentPlayer+=1;
+        round.start(players);
     }
 
     @Override
-    public void ChooseWinner(IPlayer[] players) {
+    public void ChooseWinner(Map players) {
         Map<String,Integer> players2 = new HashMap<String, Integer>();
         players2.put("Tomy",25);
         players2.put("Tomy2",67);
-        System.out.println(Collections.max(players2) + " Võittis!!");
+        Map.Entry<String,Integer> maxEntry = null;
+        for (Map.Entry<String,Integer> entry : players2.entrySet()){
+            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
+            {
+                maxEntry = entry;
+            }
+        }
+        System.out.println(maxEntry + " Võittis!!");
     }
 }
