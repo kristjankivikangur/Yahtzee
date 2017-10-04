9707754b2a6d9d3bebaa68992d008f4cfd721da5
Tomy Kalm <tomy.kalm@gmail.com>
2017-10-04 09:34:50 +0300

    Added an example of how to get HashMap's keys and values

--- src/ee/tthk/impls/game/Game.java
+++ src/ee/tthk/impls/game/Game.java
@@ -1,5 +1,6 @@
 package ee.tthk.impls.game;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 import ee.tthk.interfaces.*;
 import ee.tthk.mocks.player.Player;
 import ee.tthk.mocks.round.Round;
@@ -14,10 +15,9 @@ public class Game implements IGame {
     int NrOfPlayers;
     Round round = new Round();
     Player names = new Player();
-    Map<String,Integer> players = new HashMap<String, Integer>();
 
     @Override
-    public void StartGame(int NumberOfPlayers) {
+    public void StartGame(int NumberOfPlayers, Map<String,Integer> players) {
         for (int i = 0;i < NumberOfPlayers;i++){
             players.put(names.SetName(),0);
         }
@@ -25,10 +25,10 @@ public class Game implements IGame {
     }
 
     @Override
-    public void ChangeRound() {
+    public void ChangeRound(Map<String,Integer> players) {
         if (CurrentRounds > 13){
             if (CurrentPlayer < NrOfPlayers){
-                ChangePlayer();
+                ChangePlayer(players);
             }
             else {
                 CurrentPlayer = 1;
@@ -41,18 +41,24 @@ public class Game implements IGame {
     }
 
     @Override
-    public void ChangePlayer() {
+    public void ChangePlayer(Map<String,Integer> players) {
+
+        /* Example of how to get HashMap key aka String and how to get it's value aka integer
+        Object key = players.keySet().toArray()[1];
+        Object value = players.get(key);
+        for (String key2 : players.keySet()){
+            System.out.println(key2);
+        }
+        System.out.println("See on indeksil: 2 ja ta on " + value);
+        */
         CurrentPlayer+=1;
         round.start(players);
     }
 
     @Override
-    public void ChooseWinner(Map players) {
-        Map<String,Integer> players2 = new HashMap<String, Integer>();
-        players2.put("Tomy",25);
-        players2.put("Tomy2",67);
+    public void ChooseWinner(Map<String, Integer> players) {
         Map.Entry<String,Integer> maxEntry = null;
-        for (Map.Entry<String,Integer> entry : players2.entrySet()){
+        for (Map.Entry<String,Integer> entry : players.entrySet()){
             if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
             {
                 maxEntry = entry;
