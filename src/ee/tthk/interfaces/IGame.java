592b04a7fe14580e3ec7854f167c352f1a463894
Tomy Kalm <tomy.kalm@gmail.com>
2017-10-05 12:44:05 +0300

    Cleaning

--- src/ee/tthk/interfaces/IGame.java
+++ src/ee/tthk/interfaces/IGame.java
@@ -1,10 +1,5 @@
 package ee.tthk.interfaces;
 
-import java.util.Map;
-
-/**
- * Created by opilane on 25.09.2017.
- */
 public interface IGame {
     /**
      * Asks the player for the number of players and then calls out the Player class for it to give them names
@@ -24,7 +19,6 @@ public interface IGame {
 
     /**
      * Chooses the winner of the game after all the rounds have been played
-     * @param players - An array of player
      */
     void ChooseWinner();
 }
